11318601d13e770204d7eb24d7278b9f676cf2fd
Red Fairy <agorelik2@gmail.com>
2020-08-26 22:56:59 -0400

    change to create .post route

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,4 +1,21 @@
 const fs = require("fs");
 let { v4: uuidv4 } = require("uuid");
 
-module.exports = (app) => {};
+module.exports = (app) => {
+  app.get("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf-8", (error, data) => {
+      if (error) {
+        throw error;
+      }
+      res.json(JSON.parse(data));
+    });
+  });
+
+  app.post("/api/notes", (req, res) => {
+    let noteId = uuidv4();
+    let newNote = {
+      title: req.body.title,
+      text: req.body.text,
+      id: noteId,
+    };
+};
